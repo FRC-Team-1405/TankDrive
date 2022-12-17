79563cdce013e144a40575d3e663ebcba7327e0c
AbbyGanguly <none>
2022-12-17 09:50:35 -0500

    Created pnumatics subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.TankDrive;
 import frc.robot.subsystems.DriveBase;
+import frc.robot.subsystems.Pneumatics;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -25,6 +26,8 @@ public class RobotContainer {
 private final DriveBase driveBase = new DriveBase();
 private final TankDrive tankDrive = new TankDrive(driveBase, driver::getLeftY, driver::getRightY);
 private final ArcadeDrive arcadeDrive = new ArcadeDrive(driveBase, driver::getLeftY, driver::getRightX);
+private final Pneumatics pneumatics = new Pneumatics();
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
---
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.fasterxml.jackson.databind.ser.std.NumberSerializers.DoubleSerializer;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Pneumatics extends SubsystemBase {
+  private DoubleSolenoid buzzer = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+  private DoubleSolenoid piston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+  /** Creates a new Pneumantics. */
+  public Pneumatics() {
+    piston.set(Value.kOff);
+    buzzer.set(Value.kOff);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
